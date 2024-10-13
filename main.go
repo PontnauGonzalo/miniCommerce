25728f1d3ed634efd70099dbda09c5f7c8e46e6a
gpontnau <gpontnau@frba.utn.edu.ar>
2024-10-12 22:18:47 -0300

    Implement e-commerce functionality with User, Product, and Car structures

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
-package commerce
+package main
 
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/PontnauGonzalo/miniCommerce"
+	"github.com/PontnauGonzalo/miniCommerce/commerce"
 )
 
 type User struct {
@@ -36,4 +36,29 @@ func main() {
 	user.Display()
 	user.UpdateName("Gonzalo Pontnau")
 	user.Display()
+
+	product1 := commerce.Product{
+		ID:    1,
+		Name:  "Product 1",
+		Type:  commerce.Type{ID: 1, Code: "A", Description: "Air frier"},
+		Count: 2,
+		Price: 10.5,
+		Tags:  []string{"tag1", "tag2"},
+	}
+	product2 := commerce.Product{
+		ID:    2,
+		Name:  "Product 2",
+		Type:  commerce.Type{ID: 2, Code: "B", Description: "Blender"},
+		Count: 1,
+		Price: 20.5,
+		Tags:  []string{"tag3", "tag4"},
+	}
+
+	// Create a new car and add products
+	car := commerce.NewCar(1)
+	car.AddProduct(product1, product2)
+
+	fmt.Println()
+	fmt.Printf("Total products: %d\n", len(car.Products))
+	fmt.Printf("Total car: %.2f\n", car.TotalPrice())
 }
